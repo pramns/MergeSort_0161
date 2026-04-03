f27871c4ac409479d9e9d4f0a539337ba75116b9
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-03 14:47:34 +0700

    Create Void Input

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20], B[20];
 
 int n;
+
+void input(){
+  cout << "Masukan Panjang element array : ";
+  cin >> n;
+
+  if (n <= 20)
+  {
+      break;
+  }
+  else{
+    cout << "\nMaksimal Panjang array adalah 20";
+  }
+}
